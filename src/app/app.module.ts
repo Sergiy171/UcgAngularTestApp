e84b136e5bf6987a7e8de74092d99403b8285c8a
Serhii Kondiuk <serhii.kondiuk@edvantis.com>
2023-09-20 23:22:51 +0300

    Added user-list and user-view components with default functionality. Added Data service with default functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserViewComponent
   ],
   imports: [
     BrowserModule,
